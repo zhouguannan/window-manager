6dbf614ff3addc78674a4b66e82813ac3f7392ec
Kevin Zhou <kevin@ArchLinux.localdomain>
2021-11-10 13:49:17 +0800

    try to detect key press but faild

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 xcb_connection_t *conn;
 xcb_screen_t *screen;
 const xcb_setup_t *setup;
+xcb_generic_event_t *ev;
 
 void dieout(){
     printf("Die out");
@@ -16,10 +17,18 @@ void prepare(){
     conn = xcb_connect(NULL,NULL);
     setup = xcb_get_setup(conn);
     screen = xcb_setup_roots_iterator(setup).data;
-
 }
 
 int main(){
     prepare();
+    xcb_flush(conn);
+    while(1){
+        ev = xcb_wait_for_event(conn);
+        switch(ev->response_type){
+            case XCB_KEY_PRESS:
+                printf("Keyboard Pressed");
+                break;
+        } 
+    }
     return 0;
 }
